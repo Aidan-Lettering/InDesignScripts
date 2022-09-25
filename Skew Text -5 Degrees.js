6ef7c7fba13d7901aa7e48e592b1d2b6a36f0363
Aidan-Lettering <79215604+Aidan-Lettering@users.noreply.github.com>
2022-09-25 10:35:42 -0500

    Add files via upload

---
+++ Skew Text -5 Degrees.js
@@ -0,0 +1,25 @@
+function main() {
+    var hasErrors = false,
+        selections = app.selection;
+
+    for (var i = 0; i < selections.length && !hasErrors; i++) {
+        var textFrame = selections[i] instanceof InsertionPoint ?
+            selections[i].parentTextFrames[0] :
+            selections[i];
+        hasErrors = isError(textFrame);
+
+        if (!hasErrors) {
+            textFrame.texts.firstItem().skew -= 5;
+        };
+    }
+}
+
+function isError(obj) {
+    if (!(obj instanceof TextFrame)) {
+        alert('Please select some text frames and try again');
+        return true;
+    }
+    return false;
+}
+
+main();
